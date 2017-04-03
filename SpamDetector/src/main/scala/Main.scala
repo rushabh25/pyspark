bd10db763f3aba4a6a9640202365ec6ef79ee9c6
Rushabh Shah <rshah@comscore.com>
2017-04-03 13:43:43 -0400

    basic classifier using spark ml

--- SpamDetector/src/main/scala/Main.scala
+++ SpamDetector/src/main/scala/Main.scala
@@ -1,7 +1,10 @@
 import co.theasi.plotly._
 import org.apache.spark
+import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.sql._
-import org.viz.lightning._
+import org.apache.spark.sql.functions._
+import org.apache.spark.ml.feature.{HashingTF, IDF, StopWordsRemover, Tokenizer}
+import org.apache.spark.ml.{Pipeline, PipelineModel}
 
 
 /**
@@ -10,7 +13,7 @@ import org.viz.lightning._
 object Main {
 
 
-  case class InputFileFormat (label: String, raw_data: String)
+  case class InputFileFormat (label: String, text: String)
 
   var inputFile = ""
   var outputFile = ""
@@ -18,11 +21,11 @@ object Main {
   def processArgs(args: List[String]) : Unit = {
     args match {
       case Nil => return
-      case "--spamFile" :: head :: tail => {
+      case "--inputFile" :: head :: tail => {
         inputFile = head
         processArgs(tail)
       }
-      case "--hamFile" :: head :: tail => {
+      case "--outputFile" :: head :: tail => {
         outputFile = head
         processArgs(tail)
       }
@@ -42,12 +45,57 @@ object Main {
     import spark.implicits._
 
     val raw_data = spark.sparkContext.
-      textFile("/users/rshah/Documents/git-workspace/spark/SpamDetector/smsspamcollection/SMSSpamCollection").
+      textFile(inputFile).
       map(_.split("\t")).
       map(attributes => InputFileFormat(attributes(0), attributes(1))).
       toDS()
 
-    val 
+    val raw_data_transformed = raw_data.withColumn("label_new", when($"label" === "ham", 0.0).otherwise(1.0)).
+      drop($"label").
+      withColumnRenamed("label_new", "label")
+
+    val Array(training_set, test_set) = raw_data_transformed.randomSplit(Array(0.8, 0.2))
+
+    // tokenizer
+    val tokenizer = new Tokenizer().
+      setInputCol("text").
+      setOutputCol("words")
+
+    // Stop Words Remover
+    val stopWordsRemover = new StopWordsRemover().
+      setInputCol(tokenizer.getOutputCol).
+      setOutputCol("filtered_words")
+
+    // Hashing TF
+    val hashingTF = new HashingTF().
+      setNumFeatures(50000).
+      setInputCol(stopWordsRemover.getOutputCol).
+      setOutputCol("hashed")
+
+    // IDF
+    val idf = new IDF().
+      setInputCol(hashingTF.getOutputCol).
+      setOutputCol("features")
+
+    // Logistic Regression
+    val lr = new LogisticRegression().
+      setMaxIter(10).
+      setRegParam(0.001)
+
+    // pipeline
+    val pipeline = new Pipeline().setStages(Array(tokenizer, stopWordsRemover, hashingTF, idf, lr))
+
+    val model = pipeline.fit(training_set)
+
+    val output = model.transform(test_set)
+
+    output.write.format("com.databricks.spark.csv").mode("overwrite").option("delimiter", "\t").
+      save(outputFile)
+
+    // around 97.09% accuracy
+    //output.count = 1196
+    //output.filter($"label" === $"prediction").count
+    //res17: Long = 1102
 
   }
 
